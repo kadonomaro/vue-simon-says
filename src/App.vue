0a94457d0035de1d40fbab118746a991d9ff2f82
kadonomaro <denis.afer@gmail.com>
2020-08-30 18:14:11 +0700

    feat: remove unused starter files, add game sounds

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <game-component />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameComponent from './components/Game/GameComponent';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameComponent
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Game/GameComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="game">
+		<h1 class="game__title">Simon Says</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameComponent'
+}
+</script>
+
+<style lang="scss">
+	.game {
+		max-width: 800px;
+		margin: 0 auto;
+		&__title {
+			margin: 0 0 50px;
+			text-align: center;
+		}
+	}
+</style>
